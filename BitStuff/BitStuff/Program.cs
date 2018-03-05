ac1df095dff707277ac4507d9d752bab5ca292a1
Kendolf <ikatuev@yandex.ru>
2018-03-05 23:40:55 +0400

    Bits and bytes

---
+++ BitStuff/BitStuff/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitStuff
+{
+    public class BitStuff
+    {
+        public static uint BitCount(int input) //Bit Count - counts bits in a given number. Negative numbers allowed.
+        {
+            uint result = 0;
+            uint unum = unchecked((uint)input);
+            while (unum != 0)
+            {
+                result = result + (unum & 1);
+                unum >>= 1;
+            }
+            return (result);
+        }
+
+        public static string ParityControl(string input) //Parity Control - decodes string of byte-sized numbers into ASCII string without numbers with uneven amount of bits. No negative numbers allowed!
+        {
+            string result = "", curnum = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] != ' ')
+                {
+                    curnum += input[i];
+                }
+                else
+                {
+                    result += (BitCount(int.Parse(curnum)) % 2 == 0 ? Convert.ToString((char)(int.Parse(curnum) & 127)) : "");
+                    curnum = "";
+                }
+            }
+            result += (BitCount(int.Parse(curnum)) % 2 == 0 ? Convert.ToString((char)(int.Parse(curnum) & 127)) : "");
+            return (result);
+        }
+    }
+
+    public class Programm
+    {
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ BitStuff/BitStuffTests/BitStuffTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BitStuff;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitStuff.Tests
+{
+    [TestClass()]
+    public class BitStuffTests
+    {
+        [TestMethod()]
+        public void BitCountTest_1()
+        {
+            Assert.AreEqual((uint)1, BitStuff.BitCount(1));
+        }
+
+        [TestMethod()]
+        public void BitCountTest_100()
+        {
+            Assert.AreEqual((uint)3, BitStuff.BitCount(100));
+        }
+
+        [TestMethod()]
+        public void BitCountTest_neg1()
+        {
+            Assert.AreEqual((uint)32, BitStuff.BitCount(-1));
+        }
+
+        [TestMethod()]
+        public void ParityControlTest()
+        {
+            Assert.AreEqual("Ana.", BitStuff.ParityControl("65 238 236 225 46"));
+        }
+    }
+}
